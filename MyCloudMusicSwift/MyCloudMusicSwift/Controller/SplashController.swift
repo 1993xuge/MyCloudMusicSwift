3bb72c1634f731e10f018d06ec71a28b181ba8be
1993xuge <934755308@qq.com>
2020-01-09 14:26:05 +0800

    测试 使用 UserDefaults

--- MyCloudMusicSwift/MyCloudMusicSwift/Controller/SplashController.swift
+++ MyCloudMusicSwift/MyCloudMusicSwift/Controller/SplashController.swift
@@ -14,33 +14,39 @@
 import UIKit
 
 class SplashController: UIViewController {
-    
-    
+
+
     /// 加载视图成功后调用该方法
     //  类似Android中Activity的onCreate方法
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // Do any additional setup after loading the view.
-        
+
         // 延迟3s
         DispatchQueue.main.asyncAfter(deadline: .now() + 3, execute: {
-            // 延迟了3s后。
-            self.next()
-        })
+                // 延迟了3s后。
+                self.next()
+            })
+
+        //测试偏好设置
+
+        //获取偏好设置对象
+        let userDefault = UserDefaults.standard
+
+        //保存一个字符串
+        userDefault.set("w我是 爱学啊", forKey: "user_name")
+
+        var userName = userDefault.string(forKey: "user_name")
+        print("SplashController viewDidLoad : set, userName = \(userName)")
+
+        userDefault.removeObject(forKey: "user_name")
+        userName = userDefault.string(forKey: "user_name")
+        print("SplashController viewDidLoad : remove, userName = \(userName)")
     }
-    
+
     func next() {
         print("[SplashController] next")
-        
-        // 获取到 当前 应用对应的 AppDelegate对象
-        // 全局 只有一个
-        
-        //         //
-        //        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        //
-        //        appDelegate.toGuide()
-        
         AppDelegate.shared.toGuide()
     }
 }
