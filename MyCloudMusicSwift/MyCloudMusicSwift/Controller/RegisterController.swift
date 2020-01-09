a2281ef4444cf1a0d85ab89b6a072f5f0e75e764
1993xuge <934755308@qq.com>
2020-01-09 23:55:42 +0800

    注册 输入数据 格式 检查

--- MyCloudMusicSwift/MyCloudMusicSwift/Controller/RegisterController.swift
+++ MyCloudMusicSwift/MyCloudMusicSwift/Controller/RegisterController.swift
@@ -49,6 +49,36 @@ class RegisterController: BaseController {
     @IBAction func onRegisterClick(_ sender: UIButton) {
          print("RegisterController onRegisterClick")
         ToastUtil.short("注册")
+        
+        var nickName = tfNickname.text!
+        
+        //去掉字符串中前后的空格
+        let whiteSpace = NSCharacterSet.whitespacesAndNewlines
+        
+        nickName = nickName.trimmingCharacters(in: whiteSpace)
+        
+        //判断是否输入了昵称
+        if nickName.isEmpty{
+            ToastUtil.short("请输入昵称！")
+            return
+        }
+        
+        //判断昵称格式
+        if nickName.count<2 || nickName.count>15 {
+            ToastUtil.short("昵称长度不对！")
+            return
+        }
+        
+        //获取到手机号
+        var phone=tfPhone.text!
+        
+        //去掉前后的空白字符
+        phone=phone.trimmingCharacters(in: whiteSpace)
+        if phone.isEmpty {
+            ToastUtil.short("请输入手机号！")
+            return
+        }
+        
     }
     
     /// 注册协议点击
