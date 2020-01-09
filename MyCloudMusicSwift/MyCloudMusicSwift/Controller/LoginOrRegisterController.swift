0e5e8a3beca69b27dee0f2f4c82bdcda827d72ca
1993xuge <934755308@qq.com>
2020-01-09 20:06:18 +0800

    代码 设置 btn 样式

--- MyCloudMusicSwift/MyCloudMusicSwift/Controller/LoginOrRegisterController.swift
+++ MyCloudMusicSwift/MyCloudMusicSwift/Controller/LoginOrRegisterController.swift
@@ -11,11 +11,22 @@ import UIKit
 
 class LoginOrRegisterController: UIViewController {
 
+    // 登录
+    @IBOutlet weak var btnLogin: UIButton!
+
+    // 注册
+    @IBOutlet weak var btnRegister: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
-        
+
+        // 设置 边框
+        btnLogin.showColorPrimaryBorder()
+        btnRegister.showColorPrimaryBorder()
+
+        // 设置圆角
+        ViewUtil.showLargeRadius(view: btnLogin)
+        ViewUtil.showLargeRadius(view: btnRegister)
     }
 }
