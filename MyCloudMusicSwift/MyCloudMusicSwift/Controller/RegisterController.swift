6ebc7a8b67aef91def658aeec71b15e8d4a6d917
1993xuge <934755308@qq.com>
2020-01-10 20:38:37 +0800

    添加 RxSwift 框架

--- MyCloudMusicSwift/MyCloudMusicSwift/Controller/RegisterController.swift
+++ MyCloudMusicSwift/MyCloudMusicSwift/Controller/RegisterController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+import RxSwift
+
 class RegisterController: BaseController {
 
     /// 昵称控件
@@ -81,7 +83,7 @@ class RegisterController: BaseController {
             ToastUtil.short("邮箱格式不正确！")
             return
         }
-        
+
         // 检查密码
         let password = tfPassword.text!.trim()!
         if password.isEmpty {
@@ -92,22 +94,22 @@ class RegisterController: BaseController {
             ToastUtil.short("密码格式不正确！")
             return
         }
-        
+
         // 检查确认密码
-        let confirmPassword=tfConfirmPassword.text!.trim()!
-        
+        let confirmPassword = tfConfirmPassword.text!.trim()!
+
         if confirmPassword.isEmpty {
             ToastUtil.short("请输入确认密码！")
             return
         }
-        
+
         guard confirmPassword.isPassword() else {
             ToastUtil.short("确认密码格式不正确！")
             return
         }
-        
+
         //判断确认密码和密码是否一样
-        guard password==confirmPassword else {
+        guard password == confirmPassword else {
             ToastUtil.short("两次密码不一致！")
             return
         }
