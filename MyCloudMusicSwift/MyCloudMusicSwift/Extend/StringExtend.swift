d6bef5439f5ec20640486a2ff2691cfe792a1edc
1993xuge <934755308@qq.com>
2020-01-10 09:20:45 +0800

    添加 测试方法

---
+++ MyCloudMusicSwift/MyCloudMusicSwift/Extend/StringExtend.swift
@@ -0,0 +1,19 @@
+//
+//  StringExtend.swift
+//  MyCloudMusicSwift
+//
+//  Created by wangyixu on 2020/1/10.
+//  Copyright © 2020 wangyixu. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    /// 是否符合手机号格式
+    ///
+    /// - Returns: <#return value description#>
+    public func isPhone() -> Bool {
+        return true
+    }
+}
---
+++ MyCloudMusicSwift/MyCloudMusicSwiftTests/Extend/StringExtendTests.swift
@@ -0,0 +1,27 @@
+//
+//  StringExtendTests.swift
+//  MyCloudMusicSwiftTests
+//
+//  Created by wangyixu on 2020/1/10.
+//  Copyright © 2020 wangyixu. All rights reserved.
+//
+
+import XCTest
+
+class StringExtendTests: XCTestCase {
+    
+    /// 测试手机号格式
+    func testIsPhone() {
+        /// 这是一个正确的手机号格式
+        
+        /// 这里使用断言来判断结果一定为true
+        /// 只有结果为true表示通过
+        /// 也就表示我们的代码没问题
+        assert("13141111111".isPhone())
+        
+        /// 这是一个错误的手机号格式
+        XCTAssertFalse("ixuea".isPhone())
+        
+    }
+    
+}
