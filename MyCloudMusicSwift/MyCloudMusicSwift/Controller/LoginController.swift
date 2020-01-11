20c51d451e338d19f4131348db755f210dfe6cdd
1993xuge <934755308@qq.com>
2020-01-11 18:05:57 +0800

    完成 登录 界面的UI

---
+++ MyCloudMusicSwift/MyCloudMusicSwift/Controller/LoginController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginController.swift
+//  MyCloudMusicSwift
+//
+//  Created by wangyixu on 2020/1/11.
+//  Copyright © 2020 wangyixu. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: BaseController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
