1cc947a4680d28debd2a0d757ad2f457cbf905d6
1993xuge <934755308@qq.com>
2020-01-11 18:12:30 +0800

    关联 登录 界面的控件

--- MyCloudMusicSwift/MyCloudMusicSwift/Controller/LoginController.swift
+++ MyCloudMusicSwift/MyCloudMusicSwift/Controller/LoginController.swift
@@ -10,7 +10,35 @@ import UIKit
 
 class LoginController: BaseController {
 
+    /// 用户名
+    @IBOutlet weak var tfUsername: UITextField!
+
+    /// 密码
+    @IBOutlet weak var tfPassword: UITextField!
+
+    /// 登录按钮
+    @IBOutlet weak var btLogin: UIButton!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    override func initViews() {
+        navigationItem.title = "登录"
+
+        // 显示 图标
+        tfUsername.showLeftIcon(name: "LoginItemPhone")
+        tfPassword.showLeftIcon(name: "LoginItemPhone")
+        
+        // 圆角
+        ViewUtil.showLargeRadius(view: btLogin)
+    }
+
+    /// 登录按钮点击回调方法
+    ///
+    /// - Parameter sender: <#sender description#>
+    @IBAction func onLoginClick(_ sender: UIButton) {
+        print("LoginController onLoginClick")
     }
 }
