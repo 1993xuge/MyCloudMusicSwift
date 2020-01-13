0529431a83979f878b9bf5592bd0706e69eee639
1993xuge <934755308@qq.com>
2020-01-13 14:51:22 +0800

    关联 广告界面的 事件

--- MyCloudMusicSwift/MyCloudMusicSwift/Controller/AdController.swift
+++ MyCloudMusicSwift/MyCloudMusicSwift/Controller/AdController.swift
@@ -10,7 +10,30 @@ import UIKit
 
 class AdController: BaseController {
 
+    /// 跳过广告按钮
+    @IBOutlet weak var btSkipAd: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    override func initViews() {
+        super.initViews()
+
+        ViewUtil.showRadius(btSkipAd, 15)
+    }
+
+    /// 点击了广告
+    ///
+    /// - Parameter sender: <#sender description#>
+    @IBAction func onAdClick(_ sender: UIButton) {
+        print("AdController onAdClick")
+    }
+
+    /// 点击了跳过广告按钮
+    ///
+    /// - Parameter sender: <#sender description#>
+    @IBAction func onSkipAdClick(_ sender: UIButton) {
+        print("AdController onSkipAdClick")
     }
 }
--- MyCloudMusicSwift/MyCloudMusicSwift/Util/ViewUtil.swift
+++ MyCloudMusicSwift/MyCloudMusicSwift/Util/ViewUtil.swift
@@ -12,11 +12,21 @@ class ViewUtil {
     /// 设置 大圆角
     ///
     /// - Parameter view: 要设置的View
-    static func showLargeRadius(view:UIView) {
-        view.layer.cornerRadius = CGFloat(SIZE_LARGE_RADIUS)
-        
+    static func showLargeRadius(view: UIView) {
+        showRadius(view, Float(SIZE_LARGE_RADIUS))
+    }
+
+    /// 设置圆角
+    ///
+    /// - Parameters:
+    ///   - view: <#view description#>
+    ///   - radius: <#radius description#>
+    static func showRadius(_ view: UIView, _ radius: Float) {
+        view.layer.cornerRadius = CGFloat(radius)
+
         //裁剪多余的内容
-        //例如：给ImageView设置了圆角，如果不裁剪多余的内容，就不会生效
+        //例如：给ImageView设置了圆角
+        //如果不裁剪多余的内容，就不会生效
         view.clipsToBounds = true
     }
 }
