b1237a741bea4725995c596411009fc9a46328fb
1993xuge <934755308@qq.com>
2020-01-13 09:18:07 +0800

    添加 控制器

---
+++ MyCloudMusicSwift/MyCloudMusicSwift/Controller/DiscoveryController.swift
@@ -0,0 +1,20 @@
+//
+//  DiscoveryController.swift
+//  MyCloudMusicSwift
+//
+//  Created by wangyixu on 2020/1/13.
+//  Copyright © 2020 wangyixu. All rights reserved.
+//
+
+import UIKit
+
+class DiscoveryController: BaseCommonController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func initViews() {
+        navigationItem.title = "发现"
+    }
+}
