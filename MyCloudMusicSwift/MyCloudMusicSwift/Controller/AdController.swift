f7bb6c030cfe176d222dcb221c7222cede2c39d4
1993xuge <934755308@qq.com>
2020-01-13 22:38:42 +0800

    完成 广告 交互

--- MyCloudMusicSwift/MyCloudMusicSwift/Controller/AdController.swift
+++ MyCloudMusicSwift/MyCloudMusicSwift/Controller/AdController.swift
@@ -13,6 +13,8 @@ class AdController: BaseController {
     /// 跳过广告按钮
     @IBOutlet weak var btSkipAd: UIButton!
 
+    var task: DispatchWorkItem!
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -29,6 +31,30 @@ class AdController: BaseController {
         return UIStatusBarStyle.lightContent
     }
 
+    override func initDatas() {
+        super.initDatas()
+
+        //TODO 设置广告
+        //如果通过服务端获取到广告图片
+        //可以在这设置到ImageView
+
+        //创建一个闭包
+        //如果要取消一个延时就要使用这种方式创建
+        task = DispatchWorkItem {
+            self.next()
+        }
+
+        //添加任务到队列
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3, execute: task)
+    }
+
+    /// 进入首页
+    func next() {
+        print("AdController next")
+
+        AppDelegate.shared.toHome()
+    }
+
     /// 点击了广告
     ///
     /// - Parameter sender: <#sender description#>
@@ -41,5 +67,11 @@ class AdController: BaseController {
     /// - Parameter sender: <#sender description#>
     @IBAction func onSkipAdClick(_ sender: UIButton) {
         print("AdController onSkipAdClick")
+
+        //取消延时
+        task.cancel()
+
+        //马上进入下一步操作
+        next()
     }
 }
