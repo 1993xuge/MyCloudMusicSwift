b6e58cfdf888c05c55480607586fe237a75353d7
1993xuge <934755308@qq.com>
2020-01-14 09:23:35 +0800

    添加 CollectionView

---
+++ MyCloudMusicSwift/MyCloudMusicSwift/Cell/TitleCell.swift
@@ -0,0 +1,21 @@
+//
+//  TitleCell.swift
+//  MyCloudMusicSwift
+//
+//  Created by wangyixu on 2020/1/14.
+//  Copyright © 2020 wangyixu. All rights reserved.
+//
+
+import UIKit
+
+class TitleCell: UICollectionViewCell {
+
+    /// 当前Cell的名称
+    static let NAME = "TitleCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- MyCloudMusicSwift/MyCloudMusicSwift/Controller/DiscoveryController.swift
+++ MyCloudMusicSwift/MyCloudMusicSwift/Controller/DiscoveryController.swift
@@ -10,12 +10,20 @@ import UIKit
 
 class DiscoveryController: BaseCommonController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     override func initViews() {
         navigationItem.title = "发现"
+        
+        //
+        let titleNib = UINib(nibName: TitleCell.NAME, bundle: nil)
+        
+        // 注册Cell
+        collectionView.register(titleNib, forCellWithReuseIdentifier: TitleCell.NAME)
     }
 
     override func initListeners() {
@@ -35,7 +43,33 @@ class DiscoveryController: BaseCommonController {
     @objc func onAdClick(notification: NSNotification) {
         let adUri = notification.userInfo!["adUri"] as! String
         print("DiscoveryController onAdClick:\(adUri)")
-        
+
         WebController.start(navigationController!, "活动详情", adUri)
+    }
+}
+
+extension DiscoveryController: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    /// 返回有多个条目
+    ///
+    /// - Parameters:
+    ///   - collectionView: <#collectionView description#>
+    ///   - section: <#section description#>
+    /// - Returns: <#return value description#>
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    /// 返回当前位置的Cell
+    ///
+    /// - Parameters:
+    ///   - collectionView: <#collectionView description#>
+    ///   - indexPath: <#indexPath description#>
+    /// - Returns: <#return value description#>
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TitleCell.NAME, for: indexPath)
+        
+        return cell
     }
 }
